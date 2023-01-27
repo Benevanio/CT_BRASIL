d59eb5114c96c26e2f7795bfb8939ab775c60463
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-26 21:29:51 -0300

    Restart the project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import logo from './logo.svg';
+import './index.css';
 
-import './App.scss';
-import Navbar from './components/Navbar/Navbar';
-import TourList from './components/Tour/index';
 function App() {
   return (
-    <>
-    <Navbar />
-    <TourList />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
 export default App;
