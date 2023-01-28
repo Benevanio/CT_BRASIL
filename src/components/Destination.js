7c1738ea43814be32b09220f5819a1debd3f2997
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-28 15:06:13 -0300

    System

--- src/components/Destination.js
+++ src/components/Destination.js
@@ -8,7 +8,44 @@ const Destination = () => {
             <p>
                 Conheça os destinos mais visitados no estado de Sergipe e se apaixone.
             </p>
-            <Destinatio />
+            <Destinatio
+                Heading="Porto Da Folha"
+                Text=" Em novembro de 1807, 
+            Antônio Gomes Ferrão de Castelo Branco
+            registrou seus títulos imobiliários na Câmara de Propriá,
+            declarando ser de 30 léguas a extensão de suas terras, 
+            latifúndio que constituiu o morgado de Porto da Folha.
+            Porém, quem colonizou as terras de Porto da 
+            Folha foi Tomás de Bermudes, fundando um curral e fazendo amizade 
+            com os índios. “A fazenda Curral do Buraco originou a povoação do
+            Buraco, que em 19 de fevereiro de 1841 passou a se chamar 
+            Nossa Senhora da Conceição de Porto da Folha”, 
+            informa a Enciclopédia. Até hoje quem nasce 
+            em Porto da Folha é conhecido por buraqueiro. 
+            Os moradores mais antigos acreditam que o nome Buraco
+            surgiu porque a cidade é cercada de morros, 
+            dando a impressão de que fica em uma baixada."
+            Heading2="Caninde De São Francisco"
+            Text2="O município foi palco de muitas cenas da minissérie Amores Roubados e da novela Cordel Encantado,ambas exibidas na Rede Globo.
+            Mas a localidade tornou-se massivamente conhecida ao servir de cenário e caracterizar ambientação-base do enredo da telenovela Velho Chico,
+            também exibida pela Globo.
+            Cidade que tem uma prainha famosa,com intuito de atender a crescente demanda turística, no ano de 2016 a localidade passou
+            a contar com a Orla Salomão Porfírio Britto para a comodidade tanto de seus habitantes locais como
+            também para a apreciação da magnífica vista do emblemático Rio São Francisco como presente aos seus visitantes.
+        "
+            Heading3="Aracaju"
+            Text3 ="É apontada como a capital com menor desigualdade do Nordeste Brasileiro,
+            como a cidade com os hábitos de vida mais saudáveis do país, exemplo nacional na consideração de ciclovias nos projetos de
+            deslocamento urbano e é considerada a segunda capital do país com menor índice de fumantes.
+            Está entre as capitais com os custos de vida mais reduzidos do país,
+            tendo focado mais recentemente suas ações turísticas na criação de alojamentos coletivos,
+            tais como os mundialmente conhecidos hostels.
+            As terras onde hoje se encontra Aracaju originaram-se de sesmarias doadas a Pero Gonçalves por volta do ano de 1602.
+            No ano de 1699, tem-se notícia de um povoado surgido às margens do Rio Sergipe, próximo à região onde este deságua no mar,
+            com o nome de Santo Antônio de Aracaju.
+        "
+
+            />
         </div>
     );
 }
--- src/components/DestineData.js
+++ src/components/DestineData.js
@@ -6,53 +6,24 @@ class DestineData extends Component {
             <>
             <div className="Firts">
                 <div className="Desc_text">
-                    <h2>Rainha das vaquejadas</h2>
+                    <h2>{this.props.Heading}</h2>
                     <p>
-                        Porto da Folha é um município brasileiro do estado de
-                        Sergipe. Localiza-se a uma latitude 10º30'30" sul e a uma
-                        longitude 37º30'00" oeste, estando a uma altitude de 10
-                        metros. Sua população estimada em 2010 era de 4.000
-                        habitantes.
+                    {this.props.Text}
                     </p>
+                    <small>Fonte: Wikipedia</small>
                 </div>
                 <div className="Img">
                     <img src="https://user-images.githubusercontent.com/78994881/215268830-ba825bbd-1d71-433a-96b4-5e865a7174b2.jpg" alt='Porto_da_folha'/>
                     <img src="https://user-images.githubusercontent.com/78994881/215268833-1065bcb9-463c-414f-bcd8-de54121a736e.jpg" alt='Porto_da Folha'></img> </div>
             </div>
+            
             <div className="Firts">
                 <div className="Desc_text">
-                    <h2>Porto Da Folha</h2>
+                    <h2>{this.props.Heading2}</h2>
                     <p>
-                        Porto da Folha é um município brasileiro do estado de
-                        Sergipe, O Agreste sergipe e a minha terra natal.
-                        Porto da Folha é conhecida como a Rainha das Vaquejadas.
-                        Em novembro de 1807, Antônio Gomes Ferrão de Castelo Branco registrou seus títulos imobiliários na Câmara de Propriá,
-                        declarando ser de 30 léguas a extensão de suas terras,
-                        latifúndio que constituiu o morgado de Porto da Folha.
-
-                        Porém, quem colonizou as terras de Porto da Folha foi Tomás de Bermudes, fundando um curral e fazendo amizade com os índios.
-                        “A fazenda Curral do Buraco originou a povoação do Buraco,
-                        que em 19 de fevereiro de 1841 passou a se chamar Nossa Senhora da Conceição de Porto da Folha”,
-                        informa a Enciclopédia.
-                        Até hoje quem nasce em Porto da Folha é conhecido por buraqueiro.
+                        {this.props.Text2}
                     </p>
-                    <small>Fonte: Wikipedia</small>
-                </div>
-                <div className="Img">
-                    <img src="https://user-images.githubusercontent.com/78994881/215268830-ba825bbd-1d71-433a-96b4-5e865a7174b2.jpg" alt='Porto_da_folha' />
-                    <img src="https://user-images.githubusercontent.com/78994881/215268833-1065bcb9-463c-414f-bcd8-de54121a736e.jpg" alt='Porto_da Folha'></img> </div>
-            </div>
-            <div className="Firts">
-                <div className="Desc_text">
-                    <h2>Caninde De São Francisco</h2>
-                    <p>
-                        O município foi palco de muitas cenas da minissérie Amores Roubados e da novela Cordel Encantado,ambas exibidas na Rede Globo.
-                        Mas a localidade tornou-se massivamente conhecida ao servir de cenário e caracterizar ambientação-base do enredo da telenovela Velho Chico,
-                        também exibida pela Globo.
-                        Cidade que tem uma prainha famosa,com intuito de atender a crescente demanda turística, no ano de 2016 a localidade passou
-                        a contar com a Orla Salomão Porfírio Britto para a comodidade tanto de seus habitantes locais como
-                        também para a apreciação da magnífica vista do emblemático Rio São Francisco como presente aos seus visitantes.</p>
-                    <small>Fonte: Wikipedia</small>
+                        <small>Fonte: Wikipedia</small>
                 </div>
                 <div className="Img">
                     <img src="https://user-images.githubusercontent.com/78994881/215278806-50ad6d27-941c-457b-94bd-478987d0b8da.jpg" alt='Porto_da_folha' />
@@ -60,17 +31,9 @@ class DestineData extends Component {
             </div>
             <div className="Firts">
                 <div className="Desc_text">
-                    <h2>Aracaju</h2>
+                    <h2>{this.props.Heading3}</h2>
                     <p>
-                        É apontada como a capital com menor desigualdade do Nordeste Brasileiro,
-                        como a cidade com os hábitos de vida mais saudáveis do país, exemplo nacional na consideração de ciclovias nos projetos de
-                        deslocamento urbano e é considerada a segunda capital do país com menor índice de fumantes.
-                        Está entre as capitais com os custos de vida mais reduzidos do país,
-                        tendo focado mais recentemente suas ações turísticas na criação de alojamentos coletivos,
-                        tais como os mundialmente conhecidos hostels.
-                        As terras onde hoje se encontra Aracaju originaram-se de sesmarias doadas a Pero Gonçalves por volta do ano de 1602.
-                        No ano de 1699, tem-se notícia de um povoado surgido às margens do Rio Sergipe, próximo à região onde este deságua no mar,
-                        com o nome de Santo Antônio de Aracaju.
+                        {this.props.Text3}
                     </p>
                     <small>Fonte: Wikipedia</small>
                 </div>
